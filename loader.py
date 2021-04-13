bb8beb1358e3e14ffb79611d69364ed832f079aa
Yang Liu <thomasliuy@gmail.com>
2021-04-13 15:59:13 -0400

    datasets with substructs

--- loader.py
+++ loader.py
@@ -1,12 +1,11 @@
 import os
+import rdkit
 import torch
 import pickle
-from itertools import chain
+from itertools import chain, product
 
 import pandas as pd
 import numpy as np
-import networkx as nx
-from rdkit import Chem
 from rdkit.Chem import Descriptors
 from rdkit.Chem import AllChem
 from rdkit.Chem.rdMolDescriptors import GetMorganFingerprintAsBitVect
@@ -14,256 +13,12 @@ from torch.utils import data
 from torch_geometric.data import Data
 from torch_geometric.data import InMemoryDataset
 
-from .util import get_substructs
-
-
-# allowable node and edge features
-allowable_features = {
-    "possible_atomic_num_list": list(range(1, 119)),
-    "possible_formal_charge_list": [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5],
-    "possible_chirality_list": [
-        Chem.rdchem.ChiralType.CHI_UNSPECIFIED,
-        Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW,
-        Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW,
-        Chem.rdchem.ChiralType.CHI_OTHER,
-    ],
-    "possible_hybridization_list": [
-        Chem.rdchem.HybridizationType.S,
-        Chem.rdchem.HybridizationType.SP,
-        Chem.rdchem.HybridizationType.SP2,
-        Chem.rdchem.HybridizationType.SP3,
-        Chem.rdchem.HybridizationType.SP3D,
-        Chem.rdchem.HybridizationType.SP3D2,
-        Chem.rdchem.HybridizationType.UNSPECIFIED,
-    ],
-    "possible_numH_list": [0, 1, 2, 3, 4, 5, 6, 7, 8],
-    "possible_implicit_valence_list": [0, 1, 2, 3, 4, 5, 6],
-    "possible_degree_list": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    "possible_bonds": [
-        Chem.rdchem.BondType.SINGLE,
-        Chem.rdchem.BondType.DOUBLE,
-        Chem.rdchem.BondType.TRIPLE,
-        Chem.rdchem.BondType.AROMATIC,
-    ],
-    "possible_bond_dirs": [  # only for double bond stereo information
-        Chem.rdchem.BondDir.NONE,
-        Chem.rdchem.BondDir.ENDUPRIGHT,
-        Chem.rdchem.BondDir.ENDDOWNRIGHT,
-    ],
-}
-
-
-def mol_to_graph_data_obj_simple(mol):
-    """
-    Converts rdkit mol object to graph Data object required by the pytorch
-    geometric package. NB: Uses simplified atom and bond features, and represent
-    as indices
-    :param mol: rdkit mol object
-    :return: graph data object with the attributes: x, edge_index, edge_attr
-    """
-    # atoms, num_atom_features = 2
-    atom_features_list = []
-    for atom in mol.GetAtoms():
-        atom_feature = [
-            allowable_features["possible_atomic_num_list"].index(atom.GetAtomicNum())
-        ] + [allowable_features["possible_chirality_list"].index(atom.GetChiralTag())]
-        atom_features_list.append(atom_feature)
-    x = torch.tensor(np.array(atom_features_list), dtype=torch.long)
-
-    # bonds
-    num_bond_features = 2  # bond type, bond direction
-    if len(mol.GetBonds()) > 0:  # mol has bonds
-        edges_list = []
-        edge_features_list = []
-        for bond in mol.GetBonds():
-            i = bond.GetBeginAtomIdx()
-            j = bond.GetEndAtomIdx()
-            edge_feature = [
-                allowable_features["possible_bonds"].index(bond.GetBondType())
-            ] + [allowable_features["possible_bond_dirs"].index(bond.GetBondDir())]
-            edges_list.append((i, j))
-            edge_features_list.append(edge_feature)
-            edges_list.append((j, i))
-            edge_features_list.append(edge_feature)
-
-        # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
-        edge_index = torch.tensor(np.array(edges_list).T, dtype=torch.long)
-
-        # data.edge_attr: Edge feature matrix with shape [num_edges, num_edge_features]
-        edge_attr = torch.tensor(np.array(edge_features_list), dtype=torch.long)
-    else:  # mol has no bonds
-        edge_index = torch.empty((2, 0), dtype=torch.long)
-        edge_attr = torch.empty((0, num_bond_features), dtype=torch.long)
-
-    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
-
-    return data
-
-
-def graph_data_obj_to_mol_simple(data_x, data_edge_index, data_edge_attr):
-    """
-    Convert pytorch geometric data obj to rdkit mol object. NB: Uses simplified
-    atom and bond features, and represent as indices.
-    :param: data_x:
-    :param: data_edge_index:
-    :param: data_edge_attr
-    :return:
-    """
-    mol = Chem.RWMol()
-
-    # atoms
-    atom_features = data_x.cpu().numpy()
-    num_atoms = atom_features.shape[0]
-    for i in range(num_atoms):
-        atomic_num_idx, chirality_tag_idx = atom_features[i]
-        atomic_num = allowable_features["possible_atomic_num_list"][atomic_num_idx]
-        chirality_tag = allowable_features["possible_chirality_list"][chirality_tag_idx]
-        atom = Chem.Atom(atomic_num)
-        atom.SetChiralTag(chirality_tag)
-        mol.AddAtom(atom)
-
-    # bonds
-    edge_index = data_edge_index.cpu().numpy()
-    edge_attr = data_edge_attr.cpu().numpy()
-    num_bonds = edge_index.shape[1]
-    for j in range(0, num_bonds, 2):
-        begin_idx = int(edge_index[0, j])
-        end_idx = int(edge_index[1, j])
-        bond_type_idx, bond_dir_idx = edge_attr[j]
-        bond_type = allowable_features["possible_bonds"][bond_type_idx]
-        bond_dir = allowable_features["possible_bond_dirs"][bond_dir_idx]
-        mol.AddBond(begin_idx, end_idx, bond_type)
-        # set bond direction
-        new_bond = mol.GetBondBetweenAtoms(begin_idx, end_idx)
-        new_bond.SetBondDir(bond_dir)
-
-    # Chem.SanitizeMol(mol) # fails for COC1=CC2=C(NC(=N2)[S@@](=O)CC2=NC=C(
-    # C)C(OC)=C2C)C=C1, when aromatic bond is possible
-    # when we do not have aromatic bonds
-    # Chem.SanitizeMol(mol, sanitizeOps=Chem.SanitizeFlags.SANITIZE_KEKULIZE)
-
-    return mol
-
-
-def graph_data_obj_to_nx_simple(data):
-    """
-    Converts graph Data object required by the pytorch geometric package to
-    network x data object. NB: Uses simplified atom and bond features,
-    and represent as indices. NB: possible issues with recapitulating relative
-    stereochemistry since the edges in the nx object are unordered.
-    :param data: pytorch geometric Data object
-    :return: network x object
-    """
-    G = nx.Graph()
-
-    # atoms
-    atom_features = data.x.cpu().numpy()
-    num_atoms = atom_features.shape[0]
-    for i in range(num_atoms):
-        atomic_num_idx, chirality_tag_idx = atom_features[i]
-        G.add_node(i, atom_num_idx=atomic_num_idx, chirality_tag_idx=chirality_tag_idx)
-        pass
-
-    # bonds
-    edge_index = data.edge_index.cpu().numpy()
-    edge_attr = data.edge_attr.cpu().numpy()
-    num_bonds = edge_index.shape[1]
-    for j in range(0, num_bonds, 2):
-        begin_idx = int(edge_index[0, j])
-        end_idx = int(edge_index[1, j])
-        bond_type_idx, bond_dir_idx = edge_attr[j]
-        if not G.has_edge(begin_idx, end_idx):
-            G.add_edge(
-                begin_idx,
-                end_idx,
-                bond_type_idx=bond_type_idx,
-                bond_dir_idx=bond_dir_idx,
-            )
-
-    return G
-
-
-def nx_to_graph_data_obj_simple(G):
-    """
-    Converts nx graph to pytorch geometric Data object. Assume node indices
-    are numbered from 0 to num_nodes - 1. NB: Uses simplified atom and bond
-    features, and represent as indices. NB: possible issues with
-    recapitulating relative stereochemistry since the edges in the nx
-    object are unordered.
-    :param G: nx graph obj
-    :return: pytorch geometric Data object
-    """
-    # atoms, num_atom_features = 2, (atom type, chirality tag)
-    atom_features_list = []
-    for _, node in G.nodes(data=True):
-        atom_feature = [node["atom_num_idx"], node["chirality_tag_idx"]]
-        atom_features_list.append(atom_feature)
-    x = torch.tensor(np.array(atom_features_list), dtype=torch.long)
-
-    # bonds
-    num_bond_features = 2  # bond type, bond direction
-    if len(G.edges()) > 0:  # mol has bonds
-        edges_list = []
-        edge_features_list = []
-        for i, j, edge in G.edges(data=True):
-            edge_feature = [edge["bond_type_idx"], edge["bond_dir_idx"]]
-            edges_list.append((i, j))
-            edge_features_list.append(edge_feature)
-            edges_list.append((j, i))
-            edge_features_list.append(edge_feature)
-
-        # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
-        edge_index = torch.tensor(np.array(edges_list).T, dtype=torch.long)
-
-        # data.edge_attr: Edge feature matrix with shape [num_edges, num_edge_features]
-        edge_attr = torch.tensor(np.array(edge_features_list), dtype=torch.long)
-    else:  # mol has no bonds
-        edge_index = torch.empty((2, 0), dtype=torch.long)
-        edge_attr = torch.empty((0, num_bond_features), dtype=torch.long)
-
-    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
-
-    return data
-
-
-def get_gasteiger_partial_charges(mol, n_iter=12):
-    """
-    Calculates list of gasteiger partial charges for each atom in mol object.
-    :param mol: rdkit mol object
-    :param n_iter: number of iterations. Default 12
-    :return: list of computed partial charges for each atom.
-    """
-    Chem.rdPartialCharges.ComputeGasteigerCharges(
-        mol, nIter=n_iter, throwOnParamFailure=True
-    )
-    partial_charges = [float(a.GetProp("_GasteigerCharge")) for a in mol.GetAtoms()]
-    return partial_charges
-
-
-def create_standardized_mol_id(smiles):
-    """
-
-    :param smiles:
-    :return: inchi
-    """
-    if check_smiles_validity(smiles):
-        # remove stereochemistry
-        smiles = AllChem.MolToSmiles(
-            AllChem.MolFromSmiles(smiles), isomericSmiles=False
-        )
-        mol = AllChem.MolFromSmiles(smiles)
-        if mol is not None:
-            if "." in smiles:  # if multiple species, pick largest molecule
-                mol_species_list = split_rdkit_mol_obj(mol)
-                largest_mol = get_largest_mol(mol_species_list)
-                inchi = AllChem.MolToInchi(largest_mol)
-            else:
-                inchi = AllChem.MolToInchi(mol)
-            return inchi
-        else:
-            return
-    else:
-        return
+from .util import (
+    get_substructs,
+    mol_to_graph_data_obj_simple,
+    split_rdkit_mol_obj,
+    get_largest_mol,
+)
 
 
 class MoleculeDataset(InMemoryDataset):
@@ -305,16 +60,18 @@ class MoleculeDataset(InMemoryDataset):
     def download(self):
         pass
 
-    def add_data_to_list(smiles, mols, labels, data_list, data_smiles_list):
+    def add_data_to_list(self, smiles, mols, labels, data_list, data_smiles_list):
         for i in range(len(smiles)):
             rdkit_mol = mols[i]
+            if rdkit_mol is None:
+                continue
             data = mol_to_graph_data_obj_simple(rdkit_mol)
             data.id = torch.tensor([i])
-            if labels.dim() > 1:
+            if len(labels.shape) > 1:
                 data.y = torch.tensor(labels[i, :])
             else:
-                data.y = torch.tensor(labels[i])
-            data.substucts = torch.tensor(get_substructs(rdkit_mol))
+                data.y = torch.tensor([labels[i]])
+            data.substructs = get_substructs(rdkit_mol)
             data_list.append(data)
             data_smiles_list.append(smiles[i])
 
@@ -694,7 +451,7 @@ def _load_bace_dataset(input_path):
     assert len(smiles_list) == len(rdkit_mol_objs_list)
     assert len(smiles_list) == len(labels)
     assert len(smiles_list) == len(folds)
-    return smiles_list, rdkit_mol_objs_list, folds.values, labels.values
+    return smiles_list, rdkit_mol_objs_list, labels.values
 
 
 def _load_bbbp_dataset(input_path):
@@ -947,43 +704,6 @@ def _load_chembl_with_labels_dataset(root_path):
     return smiles_list, preprocessed_rdkitArr, folds, denseOutputData
 
 
-def check_smiles_validity(smiles):
-    m = Chem.MolFromSmiles(smiles)
-    if m:
-        return True
-    else:
-        return False
-
-
-def split_rdkit_mol_obj(mol):
-    """
-    Split rdkit mol object containing multiple species or one species into a
-    list of mol objects or a list containing a single object respectively
-    :param mol:
-    :return:
-    """
-    smiles = AllChem.MolToSmiles(mol, isomericSmiles=True)
-    smiles_list = smiles.split(".")
-    mol_species_list = []
-    for s in smiles_list:
-        if check_smiles_validity(s):
-            mol_species_list.append(AllChem.MolFromSmiles(s))
-    return mol_species_list
-
-
-def get_largest_mol(mol_list):
-    """
-    Given a list of rdkit mol objects, returns mol object containing the
-    largest num of atoms. If multiple containing largest num of atoms,
-    picks the first one
-    :param mol_list:
-    :return:
-    """
-    num_atoms_list = [len(m.GetAtoms()) for m in mol_list]
-    largest_mol_idx = num_atoms_list.index(max(num_atoms_list))
-    return mol_list[largest_mol_idx]
-
-
 def create_all_datasets():
     # create dataset
     downstream_dir = [
@@ -999,14 +719,14 @@ def create_all_datasets():
 
     for dataset_name in downstream_dir:
         print(dataset_name)
-        root = "dataset/" + dataset_name
+        root = "contextSub/dataset/" + dataset_name
         dataset = MoleculeDataset(root, dataset=dataset_name)
         print(dataset)
 
-    dataset = MoleculeDataset(root="dataset/chembl", dataset="chembl")
+    dataset = MoleculeDataset(root="contextSub/dataset/chembl", dataset="chembl")
     print(dataset)
     dataset = MoleculeDataset(
-        root="dataset/zinc_standard_agent", dataset="zinc_standard_agent"
+        root="contextSub/dataset/zinc_standard_agent", dataset="zinc_standard_agent"
     )
     print(dataset)
 
--- util.py
+++ util.py
@@ -6,15 +6,299 @@ import networkx as nx
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import pandas as pd
+import numpy as np
+from torch_geometric.data import Data
 
-from .loader import MoleculeDataset
-from .loader import graph_data_obj_to_nx_simple, nx_to_graph_data_obj_simple
+
+allowable_features = {
+    "possible_atomic_num_list": list(range(1, 119)),
+    "possible_formal_charge_list": [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5],
+    "possible_chirality_list": [
+        Chem.rdchem.ChiralType.CHI_UNSPECIFIED,
+        Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW,
+        Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW,
+        Chem.rdchem.ChiralType.CHI_OTHER,
+    ],
+    "possible_hybridization_list": [
+        Chem.rdchem.HybridizationType.S,
+        Chem.rdchem.HybridizationType.SP,
+        Chem.rdchem.HybridizationType.SP2,
+        Chem.rdchem.HybridizationType.SP3,
+        Chem.rdchem.HybridizationType.SP3D,
+        Chem.rdchem.HybridizationType.SP3D2,
+        Chem.rdchem.HybridizationType.UNSPECIFIED,
+    ],
+    "possible_numH_list": [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    "possible_implicit_valence_list": [0, 1, 2, 3, 4, 5, 6],
+    "possible_degree_list": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "possible_bonds": [
+        Chem.rdchem.BondType.SINGLE,
+        Chem.rdchem.BondType.DOUBLE,
+        Chem.rdchem.BondType.TRIPLE,
+        Chem.rdchem.BondType.AROMATIC,
+    ],
+    "possible_bond_dirs": [  # only for double bond stereo information
+        Chem.rdchem.BondDir.NONE,
+        Chem.rdchem.BondDir.ENDUPRIGHT,
+        Chem.rdchem.BondDir.ENDDOWNRIGHT,
+    ],
+}
 
 
 def check_same_molecules(s1, s2):
     mol1 = AllChem.MolFromSmiles(s1)
     mol2 = AllChem.MolFromSmiles(s2)
     return AllChem.MolToInchi(mol1) == AllChem.MolToInchi(mol2)
+
+
+def check_smiles_validity(smiles):
+    m = Chem.MolFromSmiles(smiles)
+    if m:
+        return True
+    else:
+        return False
+
+
+def split_rdkit_mol_obj(mol):
+    """
+    Split rdkit mol object containing multiple species or one species into a
+    list of mol objects or a list containing a single object respectively
+    :param mol:
+    :return:
+    """
+    smiles = AllChem.MolToSmiles(mol, isomericSmiles=True)
+    smiles_list = smiles.split(".")
+    mol_species_list = []
+    for s in smiles_list:
+        if check_smiles_validity(s):
+            mol_species_list.append(AllChem.MolFromSmiles(s))
+    return mol_species_list
+
+
+def mol_to_graph_data_obj_simple(mol):
+    """
+    Converts rdkit mol object to graph Data object required by the pytorch
+    geometric package. NB: Uses simplified atom and bond features, and represent
+    as indices
+    :param mol: rdkit mol object
+    :return: graph data object with the attributes: x, edge_index, edge_attr
+    """
+    # atoms, num_atom_features = 2
+    atom_features_list = []
+    for atom in mol.GetAtoms():
+        atom_feature = [
+            allowable_features["possible_atomic_num_list"].index(atom.GetAtomicNum())
+        ] + [allowable_features["possible_chirality_list"].index(atom.GetChiralTag())]
+        atom_features_list.append(atom_feature)
+    x = torch.tensor(np.array(atom_features_list), dtype=torch.long)
+
+    # bonds
+    num_bond_features = 2  # bond type, bond direction
+    if len(mol.GetBonds()) > 0:  # mol has bonds
+        edges_list = []
+        edge_features_list = []
+        for bond in mol.GetBonds():
+            i = bond.GetBeginAtomIdx()
+            j = bond.GetEndAtomIdx()
+            edge_feature = [
+                allowable_features["possible_bonds"].index(bond.GetBondType())
+            ] + [allowable_features["possible_bond_dirs"].index(bond.GetBondDir())]
+            edges_list.append((i, j))
+            edge_features_list.append(edge_feature)
+            edges_list.append((j, i))
+            edge_features_list.append(edge_feature)
+
+        # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
+        edge_index = torch.tensor(np.array(edges_list).T, dtype=torch.long)
+
+        # data.edge_attr: Edge feature matrix with shape [num_edges, num_edge_features]
+        edge_attr = torch.tensor(np.array(edge_features_list), dtype=torch.long)
+    else:  # mol has no bonds
+        edge_index = torch.empty((2, 0), dtype=torch.long)
+        edge_attr = torch.empty((0, num_bond_features), dtype=torch.long)
+
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+
+    return data
+
+
+def graph_data_obj_to_mol_simple(data_x, data_edge_index, data_edge_attr):
+    """
+    Convert pytorch geometric data obj to rdkit mol object. NB: Uses simplified
+    atom and bond features, and represent as indices.
+    :param: data_x:
+    :param: data_edge_index:
+    :param: data_edge_attr
+    :return:
+    """
+    mol = Chem.RWMol()
+
+    # atoms
+    atom_features = data_x.cpu().numpy()
+    num_atoms = atom_features.shape[0]
+    for i in range(num_atoms):
+        atomic_num_idx, chirality_tag_idx = atom_features[i]
+        atomic_num = allowable_features["possible_atomic_num_list"][atomic_num_idx]
+        chirality_tag = allowable_features["possible_chirality_list"][chirality_tag_idx]
+        atom = Chem.Atom(atomic_num)
+        atom.SetChiralTag(chirality_tag)
+        mol.AddAtom(atom)
+
+    # bonds
+    edge_index = data_edge_index.cpu().numpy()
+    edge_attr = data_edge_attr.cpu().numpy()
+    num_bonds = edge_index.shape[1]
+    for j in range(0, num_bonds, 2):
+        begin_idx = int(edge_index[0, j])
+        end_idx = int(edge_index[1, j])
+        bond_type_idx, bond_dir_idx = edge_attr[j]
+        bond_type = allowable_features["possible_bonds"][bond_type_idx]
+        bond_dir = allowable_features["possible_bond_dirs"][bond_dir_idx]
+        mol.AddBond(begin_idx, end_idx, bond_type)
+        # set bond direction
+        new_bond = mol.GetBondBetweenAtoms(begin_idx, end_idx)
+        new_bond.SetBondDir(bond_dir)
+
+    # Chem.SanitizeMol(mol) # fails for COC1=CC2=C(NC(=N2)[S@@](=O)CC2=NC=C(
+    # C)C(OC)=C2C)C=C1, when aromatic bond is possible
+    # when we do not have aromatic bonds
+    # Chem.SanitizeMol(mol, sanitizeOps=Chem.SanitizeFlags.SANITIZE_KEKULIZE)
+
+    return mol
+
+
+def graph_data_obj_to_nx_simple(data):
+    """
+    Converts graph Data object required by the pytorch geometric package to
+    network x data object. NB: Uses simplified atom and bond features,
+    and represent as indices. NB: possible issues with recapitulating relative
+    stereochemistry since the edges in the nx object are unordered.
+    :param data: pytorch geometric Data object
+    :return: network x object
+    """
+    G = nx.Graph()
+
+    # atoms
+    atom_features = data.x.cpu().numpy()
+    num_atoms = atom_features.shape[0]
+    for i in range(num_atoms):
+        atomic_num_idx, chirality_tag_idx = atom_features[i]
+        G.add_node(i, atom_num_idx=atomic_num_idx, chirality_tag_idx=chirality_tag_idx)
+        pass
+
+    # bonds
+    edge_index = data.edge_index.cpu().numpy()
+    edge_attr = data.edge_attr.cpu().numpy()
+    num_bonds = edge_index.shape[1]
+    for j in range(0, num_bonds, 2):
+        begin_idx = int(edge_index[0, j])
+        end_idx = int(edge_index[1, j])
+        bond_type_idx, bond_dir_idx = edge_attr[j]
+        if not G.has_edge(begin_idx, end_idx):
+            G.add_edge(
+                begin_idx,
+                end_idx,
+                bond_type_idx=bond_type_idx,
+                bond_dir_idx=bond_dir_idx,
+            )
+
+    return G
+
+
+def nx_to_graph_data_obj_simple(G):
+    """
+    Converts nx graph to pytorch geometric Data object. Assume node indices
+    are numbered from 0 to num_nodes - 1. NB: Uses simplified atom and bond
+    features, and represent as indices. NB: possible issues with
+    recapitulating relative stereochemistry since the edges in the nx
+    object are unordered.
+    :param G: nx graph obj
+    :return: pytorch geometric Data object
+    """
+    # atoms, num_atom_features = 2, (atom type, chirality tag)
+    atom_features_list = []
+    for _, node in G.nodes(data=True):
+        atom_feature = [node["atom_num_idx"], node["chirality_tag_idx"]]
+        atom_features_list.append(atom_feature)
+    x = torch.tensor(np.array(atom_features_list), dtype=torch.long)
+
+    # bonds
+    num_bond_features = 2  # bond type, bond direction
+    if len(G.edges()) > 0:  # mol has bonds
+        edges_list = []
+        edge_features_list = []
+        for i, j, edge in G.edges(data=True):
+            edge_feature = [edge["bond_type_idx"], edge["bond_dir_idx"]]
+            edges_list.append((i, j))
+            edge_features_list.append(edge_feature)
+            edges_list.append((j, i))
+            edge_features_list.append(edge_feature)
+
+        # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
+        edge_index = torch.tensor(np.array(edges_list).T, dtype=torch.long)
+
+        # data.edge_attr: Edge feature matrix with shape [num_edges, num_edge_features]
+        edge_attr = torch.tensor(np.array(edge_features_list), dtype=torch.long)
+    else:  # mol has no bonds
+        edge_index = torch.empty((2, 0), dtype=torch.long)
+        edge_attr = torch.empty((0, num_bond_features), dtype=torch.long)
+
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+
+    return data
+
+
+def get_gasteiger_partial_charges(mol, n_iter=12):
+    """
+    Calculates list of gasteiger partial charges for each atom in mol object.
+    :param mol: rdkit mol object
+    :param n_iter: number of iterations. Default 12
+    :return: list of computed partial charges for each atom.
+    """
+    Chem.rdPartialCharges.ComputeGasteigerCharges(
+        mol, nIter=n_iter, throwOnParamFailure=True
+    )
+    partial_charges = [float(a.GetProp("_GasteigerCharge")) for a in mol.GetAtoms()]
+    return partial_charges
+
+
+def create_standardized_mol_id(smiles):
+    """
+
+    :param smiles:
+    :return: inchi
+    """
+    if check_smiles_validity(smiles):
+        # remove stereochemistry
+        smiles = AllChem.MolToSmiles(
+            AllChem.MolFromSmiles(smiles), isomericSmiles=False
+        )
+        mol = AllChem.MolFromSmiles(smiles)
+        if mol is not None:
+            if "." in smiles:  # if multiple species, pick largest molecule
+                mol_species_list = split_rdkit_mol_obj(mol)
+                largest_mol = get_largest_mol(mol_species_list)
+                inchi = AllChem.MolToInchi(largest_mol)
+            else:
+                inchi = AllChem.MolToInchi(mol)
+            return inchi
+        else:
+            return
+    else:
+        return
+
+
+def get_largest_mol(mol_list):
+    """
+    Given a list of rdkit mol objects, returns mol object containing the
+    largest num of atoms. If multiple containing largest num of atoms,
+    picks the first one
+    :param mol_list:
+    :return:
+    """
+    num_atoms_list = [len(m.GetAtoms()) for m in mol_list]
+    largest_mol_idx = num_atoms_list.index(max(num_atoms_list))
+    return mol_list[largest_mol_idx]
 
 
 class NegativeEdge:
@@ -302,9 +586,3 @@ def get_substructs(
         if len(matches) > 0:
             substructs.append(matches)
     return substructs
-
-
-if __name__ == "__main__":
-    transform = NegativeEdge()
-    dataset = MoleculeDataset("dataset/tox21", dataset="tox21")
-    transform(dataset[0])
